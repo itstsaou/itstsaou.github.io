a92508e7d92fb34f267e161497ed86e62c8a3a9a
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2024-03-21 13:22:32 -0400

    feat: add redirects for thai night

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,9 @@
 const path = require(`path`);
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
+  const { createPage, createRedirect } = actions;
+
+  // Create a page for an event.
   const eventTemplate = path.resolve(`./src/templates/event.js`);
   const result = await graphql(`
     query {
@@ -28,4 +30,20 @@ exports.createPages = async ({ graphql, actions }) => {
       context: { id: node.id },
     });
   });
+
+  // Create redirects for 2024 Thai Night.
+  const thaiNight2024Aliases = ["/2024-thai-night", "/thai-night"];
+  thaiNight2024Aliases.forEach((alias) =>
+    createRedirect({
+      fromPath: alias,
+      toPath: `/events/2024-04-14-thai-night`,
+      redirectInBrowser: true,
+    }),
+  );
+};
+
+exports.onPostBuild = ({ store }) => {
+  const { redirects } = store.getState();
+
+  console.log(redirects);
 };
