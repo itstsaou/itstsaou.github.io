e1ebe90ddd071f6a861af2415c5794bd9af8ff2c
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2024-03-21 23:26:43 -0400

    fix: the ticket ad on the index page not visible on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const IndexPage = () => {
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          <div className="flex justify-center sm:mb-8">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Get your ticket for 2024 Thai Night!{" "}
               <Link
